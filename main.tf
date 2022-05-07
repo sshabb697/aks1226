63f6eba3166d6109c3c6781da1170fbebdb0c12e
sshabb697 <38155111+sshabb697@users.noreply.github.com>
2022-05-08 01:09:14 +0530

    Update main.tf
    updated disk type standard

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   os_disk {
     name                 = "myOsDisk"
     caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
+    storage_account_type = "StandardSSD_LRS"
   }
 
   source_image_reference {
